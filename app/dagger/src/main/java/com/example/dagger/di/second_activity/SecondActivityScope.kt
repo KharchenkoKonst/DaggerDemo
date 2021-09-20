f39df11636786868c1536bb3a06683ed86cddec5
Константин Харченко <kharshenko.kos@gmail.com>
2021-09-21 01:27:25 +0300

    Scope demo

--- app/dagger/src/main/java/com/example/dagger/di/second_activity/SecondActivityScope.kt
+++ app/dagger/src/main/java/com/example/dagger/di/second_activity/SecondActivityScope.kt
@@ -3,6 +3,12 @@ package com.example.dagger.di.second_activity
 import javax.inject.Scope
 import kotlin.annotation.AnnotationRetention.RUNTIME
 
+/**
+ * @Retention позволяет указать жизненный цикл аннотации:
+ * Будет ли она только в исходном коде (SOURCE)
+ * В скомпилированном файле (CLASS{java}/BINARY{kotlin})
+ * Видна в процессе выполнения (RUNTIME)
+ */
 @Scope
 @Retention(RUNTIME)
 annotation class SecondActivityScope
